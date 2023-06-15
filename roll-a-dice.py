0f184bb36260355026e45c51175600199c9891a3
IshikaDhavan <107250389+IshikaDhavan@users.noreply.github.com>
2023-06-15 17:17:38 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,47 @@
+import random
+ask = input("Do you want to roll a dice? ")
+response = 'y'
+while response == 'y':
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    elif no == 2:
+        print("[-----]")
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("[-----]")
+    elif no == 3:
+        print("[-----]")
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+        print("[-----]")
+    elif no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    elif no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    elif no == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+    response = input("Enter 'y' to continue or 'n' to exit: ")
+    if response == "n":
+        print("Exiting the program...")
+        break
+
+
